1b50d53ff74e9a2ce84d4858a30f2d266985dcac
R0DN8 <98967406+R0DN8@users.noreply.github.com>
2022-02-16 16:19:08 +0100

    bg animiert

--- Space Emperor/Space.java
+++ Space Emperor/Space.java
@@ -11,23 +11,36 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot und MouseInfo)
 
 public class Space extends World
 {
-    private GreenfootImage[] backgroundimages;
+    
     /**
      * Konstruktor für Objekte der Klasse Space
      * 
      */
+  
+    
+ GifImage gifImg = new GifImage("SB.gif");
+public void gifAnimation()
+{
+    setBackground(gifImg.getCurrentImage());
+}
+ 
+public void act()
+{
+    gifAnimation();
+}
+ 
     public Space()
     {    
         // Erstellt eine neue Welt mit 1280x720 Zellen und einer Zell-Größe von 1x1 Pixeln.
         super(1090, 875, 1); //Auflösung und Pixelgröße
         //private GifImage g = new GifImage("BackgroundSpace.gif");
-        
-        
+       
         //folgende Objecte werden mit der Welt geladen:
         SideBarMenu sideBarMenu = new SideBarMenu();
             addObject(new SideBarMenu(), 983, 400);
         Skilltree skilltree = new Skilltree();
             addObject(new Skilltree(), 1000, 600);
+           
     }
     /* Diese Methode verhindert, dass Objekte angezeigt werden.
      * Was genau sie anstellt, keine Ahnung. Vielleicht zeigt sie die Objekte hinter dem 
